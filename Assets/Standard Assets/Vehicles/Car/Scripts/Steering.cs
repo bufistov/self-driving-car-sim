57e52d518f662f70328e5dd26671acf264ea1def
Tariq Rafique <mrafique@salesforce.com>
2017-02-12 14:26:32 -0800

    fix joystick steering bug

--- Assets/Standard Assets/Vehicles/Car/Scripts/Steering.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/Steering.cs
@@ -80,7 +80,7 @@ namespace UnityStandardAssets.Vehicles.Car
 
 				// reset
 				mouse_hold = false;
-                H = 0;
+				H = CrossPlatformInputManager.GetAxis ("Horizontal");
             }
 				
         }
