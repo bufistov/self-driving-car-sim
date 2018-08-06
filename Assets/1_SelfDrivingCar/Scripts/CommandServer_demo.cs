898481cec5961956625ce229b76eec46eb25c7e4
Dmitry Bufistov <dmitry@midokura.com>
2018-08-06 13:41:21 +0200

    Send images in unify way
    This patch encodes all images of simulator to
    'images' hash. So both lake and demo scenes can
    be handled uniformly by server.
    
    Signed-off-by: Dmitry Bufistov <dmitry@midokura.com>

--- Assets/1_SelfDrivingCar/Scripts/CommandServer_demo.cs
+++ Assets/1_SelfDrivingCar/Scripts/CommandServer_demo.cs
@@ -8,85 +8,86 @@ using UnityEngine.SceneManagement;
 public class CommandServer_demo : MonoBehaviour
 {
     public Boolean RandomInitPosition;
-	public CarRemoteControl CarRemoteControl;
-	public Camera NorthCamera;
-	public Camera SouthCamera;
-	public Camera EastCamera;
-	public Camera WestCamera;
-	private SocketIOComponent _socket;
-	private CarController _carController;
-	private System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+    public CarRemoteControl CarRemoteControl;
+    public Camera NorthCamera;
+    public Camera SouthCamera;
+    public Camera EastCamera;
+    public Camera WestCamera;
+    private SocketIOComponent _socket;
+    private CarController _carController;
+    private System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
 
-	void Awake() {
-		int width = 160;
-		int height = 120;
-		NorthCamera.targetTexture = new RenderTexture(width, height, 24);
-		SouthCamera.targetTexture = new RenderTexture (width, height, 24);
-		EastCamera.targetTexture = new RenderTexture (width, height, 24);
-		WestCamera.targetTexture = new RenderTexture (width, height, 24);
-		_carController = CarRemoteControl.GetComponent<CarController>();
+    void Awake() {
+        int width = 160;
+        int height = 120;
+        NorthCamera.targetTexture = new RenderTexture(width, height, 24);
+        SouthCamera.targetTexture = new RenderTexture (width, height, 24);
+        EastCamera.targetTexture = new RenderTexture (width, height, 24);
+        WestCamera.targetTexture = new RenderTexture (width, height, 24);
+        _carController = CarRemoteControl.GetComponent<CarController>();
         _carController.RandomInitPosition = RandomInitPosition;
-	}
+    }
 
-	// Use this for initialization
-	void Start()
-	{
-		_socket = GameObject.Find("SocketIO").GetComponent<SocketIOComponent>();
-		_socket.On("open", OnOpen);
-		_socket.On ("reset", OnReset);
-		_socket.On ("steer", OnSteer);
-		stopWatch.Start();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        _socket = GameObject.Find("SocketIO").GetComponent<SocketIOComponent>();
+        _socket.On("open", OnOpen);
+        _socket.On ("reset", OnReset);
+        _socket.On ("steer", OnSteer);
+        stopWatch.Start();
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-	}
+    // Update is called once per frame
+    void Update()
+    {
+    }
 
-	void OnOpen(SocketIOEvent obj)
-	{
-		Debug.Log("Connection Open");
-	}
+    void OnOpen(SocketIOEvent obj)
+    {
+        Debug.Log("Connection Open");
+    }
 
-	void OnReset(SocketIOEvent obj)
-	{
-		// UnityMainThreadDispatcher.Instance ().Clear ();
-		SceneManager.LoadScene(SceneManager.GetActiveScene ().name);
-		Debug.Log("Reseting simulator ...");
-	}
+    void OnReset(SocketIOEvent obj)
+    {
+        // UnityMainThreadDispatcher.Instance ().Clear ();
+        SceneManager.LoadScene(SceneManager.GetActiveScene ().name);
+        Debug.Log("Reseting simulator ...");
+    }
 
-	void OnSteer(SocketIOEvent obj)
-	{
-		stopWatch.Stop ();
-		Debug.Log("Steering data event ... at " + stopWatch.ElapsedMilliseconds);
-		stopWatch.Start ();
-		JSONObject jsonObject = obj.data;
-		CarRemoteControl.SteeringAngle = float.Parse(jsonObject.GetField("steering_angle").str);
-		CarRemoteControl.Acceleration = float.Parse(jsonObject.GetField("throttle").str);
-		var steering_bias = 1.0f * Mathf.Deg2Rad;
-		CarRemoteControl.SteeringAngle += steering_bias;
-		EmitTelemetry(obj);
-	}
+    void OnSteer(SocketIOEvent obj)
+    {
+        stopWatch.Stop ();
+        Debug.Log("Steering data event ... at " + stopWatch.ElapsedMilliseconds);
+        stopWatch.Start ();
+        JSONObject jsonObject = obj.data;
+        CarRemoteControl.SteeringAngle = float.Parse(jsonObject.GetField("steering_angle").str);
+        CarRemoteControl.Acceleration = float.Parse(jsonObject.GetField("throttle").str);
+        var steering_bias = 1.0f * Mathf.Deg2Rad;
+        CarRemoteControl.SteeringAngle += steering_bias;
+        EmitTelemetry(obj);
+    }
 
-	void EmitTelemetry(SocketIOEvent obj)
-	{
-		UnityMainThreadDispatcher.Instance().Enqueue(() =>
-		{
-				// Collect Data from the Car
-				Dictionary<string, string> data = new Dictionary<string, string>();
-				data["steering_angle"] = _carController.CurrentSteerAngle.ToString("N4");
-				data["throttle"] = _carController.AccelInput.ToString("N4");
-				data["speed"] = _carController.CurrentSpeed.ToString("N4");
-				data["cte"] = "0.0";
-				data["north"] = Convert.ToBase64String(CameraHelper.CaptureFrame(NorthCamera));
-				data["south"] = Convert.ToBase64String(CameraHelper.CaptureFrame(SouthCamera));
-				data["east"] = Convert.ToBase64String(CameraHelper.CaptureFrame(EastCamera));
-				data["west"] = Convert.ToBase64String(CameraHelper.CaptureFrame(WestCamera));
-				stopWatch.Stop();
-				Debug.Log("Sending frame at " + stopWatch.ElapsedMilliseconds);
-				_socket.Emit("telemetry", new JSONObject(data));
-				stopWatch.Start();
-		});
-				
-	}
+    void EmitTelemetry(SocketIOEvent obj)
+    {
+        UnityMainThreadDispatcher.Instance().Enqueue(() =>
+        {
+            // Collect Data from the Car
+            Dictionary<string, JSONObject> data = new Dictionary<string, JSONObject>();
+            data["steering_angle"] = new JSONObject(_carController.CurrentSteerAngle);
+            data["throttle"] = new JSONObject(_carController.AccelInput);
+            data["speed"] = new JSONObject(_carController.CurrentSpeed);
+            data["cte"] = new JSONObject(0.0f);
+            Dictionary<string, string> images = new Dictionary<string, string>();
+            images["camera0"] = Convert.ToBase64String(CameraHelper.CaptureFrame(NorthCamera));
+            images["camera1"] = Convert.ToBase64String(CameraHelper.CaptureFrame(EastCamera));
+            images["camera2"] = Convert.ToBase64String(CameraHelper.CaptureFrame(SouthCamera));
+            images["camera3"] = Convert.ToBase64String(CameraHelper.CaptureFrame(WestCamera));
+            data["images"] = new JSONObject(images);
+            stopWatch.Stop();
+            Debug.Log("Sending frame at " + stopWatch.ElapsedMilliseconds);
+            _socket.Emit("telemetry", new JSONObject(data));
+            stopWatch.Start();
+        });
+    }
 }
--- Assets/1_SelfDrivingCar/Scripts/project_4/CommandServer_pid.cs
+++ Assets/1_SelfDrivingCar/Scripts/project_4/CommandServer_pid.cs
@@ -7,79 +7,80 @@ using UnityEngine.SceneManagement;
 
 public class CommandServer_pid : MonoBehaviour
 {
-	public CarRemoteControl CarRemoteControl;
-	public Camera FrontFacingCamera;
-	private SocketIOComponent _socket;
-	private CarController _carController;
-	private WaypointTracker_pid wpt;
+    public CarRemoteControl CarRemoteControl;
+    public Camera FrontFacingCamera;
+    private SocketIOComponent _socket;
+    private CarController _carController;
+    private WaypointTracker_pid wpt;
 
-	// Use this for initialization
-	void Start()
-	{
-		_socket = GameObject.Find("SocketIO").GetComponent<SocketIOComponent>();
-		_socket.On("open", OnOpen);
-		_socket.On ("reset", OnReset);
-		_socket.On("steer", OnSteer);
-		_socket.On("manual", onManual);
-		_carController = CarRemoteControl.GetComponent<CarController>();
+    // Use this for initialization
+    void Start()
+    {
+        _socket = GameObject.Find("SocketIO").GetComponent<SocketIOComponent>();
+        _socket.On("open", OnOpen);
+        _socket.On ("reset", OnReset);
+        _socket.On("steer", OnSteer);
+        _socket.On("manual", onManual);
+        _carController = CarRemoteControl.GetComponent<CarController>();
         _carController.RandomInitPosition = false;
-		wpt = new WaypointTracker_pid ();
-	}
+        wpt = new WaypointTracker_pid ();
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-	}
+    // Update is called once per frame
+    void Update()
+    {
+    }
 
-	void OnOpen(SocketIOEvent obj)
-	{
-		Debug.Log("Connection Open");
-	}
+    void OnOpen(SocketIOEvent obj)
+    {
+        Debug.Log("Connection Open");
+    }
 
-	// 
-	void onManual(SocketIOEvent obj)
-	{
+    //
+    void onManual(SocketIOEvent obj)
+    {
         Debug.Log("Manual driving event ...");
-		EmitTelemetry (obj);
-	}
+        EmitTelemetry (obj);
+    }
 
-	void OnReset(SocketIOEvent obj)
-	{
-		SceneManager.LoadScene("LakeTrack");
-		Debug.Log("Reseting simulator ...");
-	}
+    void OnReset(SocketIOEvent obj)
+    {
+        SceneManager.LoadScene("LakeTrack");
+        Debug.Log("Reseting simulator ...");
+    }
 
-	void OnSteer(SocketIOEvent obj)
-	{
+    void OnSteer(SocketIOEvent obj)
+    {
         Debug.Log("Steering data event ...");
-		JSONObject jsonObject = obj.data;
-		CarRemoteControl.SteeringAngle = float.Parse(jsonObject.GetField("steering_angle").str);
-		CarRemoteControl.Acceleration = float.Parse(jsonObject.GetField("throttle").str);
-		var steering_bias = 1.0f * Mathf.Deg2Rad;
-		CarRemoteControl.SteeringAngle += steering_bias;
-		EmitTelemetry(obj);
-	}
+        JSONObject jsonObject = obj.data;
+        CarRemoteControl.SteeringAngle = float.Parse(jsonObject.GetField("steering_angle").str);
+        CarRemoteControl.Acceleration = float.Parse(jsonObject.GetField("throttle").str);
+        var steering_bias = 1.0f * Mathf.Deg2Rad;
+        CarRemoteControl.SteeringAngle += steering_bias;
+        EmitTelemetry(obj);
+    }
 
-	void EmitTelemetry(SocketIOEvent obj)
-	{
-		UnityMainThreadDispatcher.Instance().Enqueue(() =>
-		{
-			print("Attempting to Send...");
-			// send only if it's not being manually driven
-			if ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.S))) {
-				_socket.Emit("telemetry", new JSONObject());
-			} else {
-				// Collect Data from the Car
-				Dictionary<string, string> data = new Dictionary<string, string>();
-				var cte = wpt.CrossTrackError (_carController);
-				data["steering_angle"] = _carController.CurrentSteerAngle.ToString("N4");
-				data["throttle"] = _carController.AccelInput.ToString("N4");
-				data["speed"] = _carController.CurrentSpeed.ToString("N4");
-				data["cte"] = cte.ToString("N4");
-				data["image"] = Convert.ToBase64String(CameraHelper.CaptureFrame(FrontFacingCamera));
-				_socket.Emit("telemetry", new JSONObject(data));
-			}
-		});
-				
-	}
+    void EmitTelemetry(SocketIOEvent obj)
+    {
+        UnityMainThreadDispatcher.Instance().Enqueue(() =>
+        {
+            print("Attempting to Send...");
+            // send only if it's not being manually driven
+            if ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.S))) {
+                _socket.Emit("telemetry", new JSONObject());
+            } else {
+                // Collect Data from the Car
+                Dictionary<string, JSONObject> data = new Dictionary<string, JSONObject>();
+                var cte = wpt.CrossTrackError (_carController);
+                data["steering_angle"] = new JSONObject(_carController.CurrentSteerAngle);
+                data["throttle"] = new JSONObject(_carController.AccelInput);
+                data["speed"] = new JSONObject(_carController.CurrentSpeed);
+                data["cte"] = new JSONObject(cte);
+                Dictionary<string, string> images = new Dictionary<string, string>();
+                images["camera0"] = Convert.ToBase64String(CameraHelper.CaptureFrame(FrontFacingCamera));
+                data["images"] = new JSONObject(images);
+                _socket.Emit("telemetry", new JSONObject(data));
+            }
+        });
+    }
 }
